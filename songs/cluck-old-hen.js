502d7444876bae42343663914d573510d42b53f3
Alexandre Morin <amorin@adobe.com>
2024-09-21 16:31:31 +0200

    Cluck old hen

---
+++ songs/cluck-old-hen.js
@@ -0,0 +1,56 @@
+registerSong({
+  name: "cluck-old-hen",
+  title: "Cluck Old Hen",
+  author: "Traditional",
+  tuning: "sawmill",
+  categories: [ "banjo"],
+  tempo: 120,
+  chords: [ ],
+  tab: [
+    { 
+      name: "A",
+      signature: "2/4",
+      measures: [
+        {
+          groups: [
+            { notes: [ "S10", "S20", "S15", "S50" ] },
+            { notes: [ "S13", "S24", "S13", "S50" ] },
+            { notes: [ "S10", "S20", "S15", "S50" ] },
+            { notes: [ "S10", "S20", "S10", "S50" ] },
+          ]
+        },
+        {
+          groups: [
+            { notes: [ "S10", "S20", "S15", "S50" ] },
+            { notes: [ "S13", "S24", "S13", "S50" ] },
+            { notes: [ "S10", "S20", "S33", "S50" ] },
+            { notes: [ "E30", "S10,S20,S30", "S50" ] },
+          ]
+        },
+
+      ]
+    },
+    { 
+      name: "B",
+      signature: "2/4",
+      measures: [
+        {
+          groups: [
+            { notes: [ "S10", "S30", "S10", "S50" ] },
+            { notes: [ "S10", "S33", "S10", "S50" ] },
+            { notes: [ "S10", "S30", "S10", "S50" ] },
+            { notes: [ "S10", "S43", "S10", "S50" ] },
+          ]
+        },
+        {
+          groups: [
+            { notes: [ "S10", "S30", "S10", "S50" ] },
+            { notes: [ "S10", "S33", "S10", "S50" ] },
+            { notes: [ "S10", "S20", "S33", "S50" ] },
+            { notes: [ "E30", "S10,S20,S30", "S50" ] },
+          ]
+        }
+      ]
+    }
+  ],
+});
